141175b4d0a88853ba9f4f6a9ffe6b6f0561fa67
Recreateorsky <73208573+Recreateorsky@users.noreply.github.com>
2021-10-03 14:28:15 +0530

    Create n’th Pentagonal Number.cpp

---
+++ C++/n’th Pentagonal Number.cpp
@@ -0,0 +1,21 @@
+
+// C program for above approach 
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+// Finding the nth Pentagonal Number 
+int pentagonalNum(int n) 
+{ 
+    return (3*n*n - n)/2; 
+} 
+  
+// Driver program to test above function 
+int main() 
+{ 
+    int n = 10; 
+    printf("10th Pentagonal Number is = %d
+",  
+                             pentagonalNum(n)); 
+  
+    return 0; 
+} 
